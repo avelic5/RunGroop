c627eab35bc5020bf591d203da2a213b12156f72
Aldin Velić <avelic5@etf.unsa.ba>
2025-06-12 15:42:56 +0200

    add elements to the navigation bar and enable views for Clubs and Races

--- Controllers/RaceController.cs
+++ Controllers/RaceController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
 
 namespace RunGroopWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var races = _context.Clubs.ToList();
+            return View(races);
         }
     }
 }
